9a882a066fd782163c91b4efb6cfb70fd4f08854
Vu Dang N <vu.dang@asiantech.vn>
2019-01-11 10:07:52 +0700

    Add api get and update user add Authenticated

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from .views import CreateUserAPIView
+from .views import CreateUserAPIView, UserRetrieveUpdateAPIView
 from . import views
 
 urlpatterns = [
     re_path(r'^create/?$', CreateUserAPIView.as_view(), name='create'),
     re_path(r'^token/?$', views.authenticate_user, name='token'),
+    re_path(r'^update/?$', UserRetrieveUpdateAPIView.as_view(), name='update'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
@@ -25,6 +26,30 @@ class CreateUserAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+
+    # Allow only authenticated users to access this url
+    permission_classes = (IsAuthenticated, )
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        # serializer to handle turning our `User` object into something that
+        # can be JSONified and sent to the client.
+        serializer = self.serializer_class(request.user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        serializer_data = request.data.get('user', {})
+
+        serializer = UserSerializer(
+            request.user, data=serializer_data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 @api_view(['POST'])
 @permission_classes([
     AllowAny,
@@ -44,6 +69,7 @@ def authenticate_user(request):
                 user_details['name'] = "%s %s" % (user.first_name,
                                                   user.last_name)
                 user_details['token'] = token
+                user_details['expire_time'] = settings.JWT_AUTH['JWT_EXPIRATION_DELTA']
                 user_logged_in.send(
                     sender=user.__class__, request=request, user=user)
                 return Response(user_details, status=status.HTTP_200_OK)
@@ -56,6 +82,6 @@ def authenticate_user(request):
                 'can not authenticate with the given credentials or the account has been deactivated'
             }
             return Response(res, status=status.HTTP_403_FORBIDDEN)
-    except KeyError:
+    except User.DoesNotExist:
         res = {'error': 'please provide a email and a password'}
-        return Response(res)
+        return Response(res, status=status.HTTP_404_NOT_FOUND)
