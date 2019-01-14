6624c9552bc3450789927a95af17abaf5f199af5
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-14 17:27:36 +0700

    add templates

--- myapp/user/handlers.py
+++ myapp/user/handlers.py
@@ -1,7 +1,7 @@
 import inject, json, logging
 
-from django.http import HttpResponse
-from django.views import View
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from myapp.user.requests import *
 from myapp.user.usecases import *
 from shared.base_handler import *
@@ -9,7 +9,7 @@ from shared.base_handler import *
 logger = logging.getLogger(__name__)
 
 
-class AuthHandler(View):
+class AuthHandler(TemplateView):
     usecase = inject.attr(UsecaseInterface)
     bh = inject.attr(BaseHandler)
 
@@ -17,10 +17,10 @@ class AuthHandler(View):
         response = self.bh.validate(UserRegister, request.POST)
         if response is not None:
             return response
+        context = {"message": "validate success"}
+        return render(request, 'base.html', context)
 
-        return HttpResponse(
-            json.dumps({"message": "validate success"}),
-            status=200,
-            content_type='application/json'
-        )
-
+    def get(self, request):
+        # response = self.bh.validate(UserRegister, request.POST)
+        context = {"message": "validate success"}
+        return render(request, 'authen/register.html', context)
