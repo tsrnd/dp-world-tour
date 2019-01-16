b2a555b14324f77ba4a318b9b6af59ebfc849a2f
Vu Dang N <vu.dang@asiantech.vn>
2019-01-16 10:57:01 +0700

    Update path login

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,4 @@
 from django.urls import path, include, re_path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
-from myapp.serializer.auth_serializer import MyAppTokenObtainPairView
 urlpatterns = [
     path('user/', include('myapp.user.urls')),
-    re_path(r'^token/$', MyAppTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    re_path(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
-    re_path(r'^token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- myapp/user/urls.py
+++ myapp/user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
 from myapp.user.handlers import AuthHandler, AdminView, UserRetrieveUpdateAPIView, TokenView
+from myapp.serializer.auth_serializer import MyAppTokenObtainPairView
 
 urlpatterns = [
     path('register', AuthHandler.as_view(), name='register'),
     re_path(r'^update/?$', UserRetrieveUpdateAPIView.as_view(), name='update'),
     path('admin-view', AdminView.as_view(), name='admin'),
-    path('token', TokenView.as_view(), name='token')
+    re_path(r'^token/?$', TokenView.as_view(), name='token'),
+    re_path(r'^login/?$', MyAppTokenObtainPairView.as_view(), name='token')
 ]
