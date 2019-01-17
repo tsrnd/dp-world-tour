7c66d8d4d0855d78c99b5efe7728fcc560554aa4
Vu Dang N <vu.dang@asiantech.vn>
2019-01-17 15:49:44 +0700

    Update base handler check validate

--- myapp/serializer/auth_serializer.py
+++ myapp/serializer/auth_serializer.py
@@ -1,41 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.utils.six import text_type
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from django.utils.translation import ugettext_lazy as _
+
 
 class UserSerializer(serializers.ModelSerializer):
 
-      date_joined = serializers.ReadOnlyField()
+    date_joined = serializers.ReadOnlyField()
 
-      class Meta(object):
+    class Meta(object):
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'date_joined',
                   'password')
         extra_kwargs = {'password': {'write_only': True}}
-        
-class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True)
-
-    default_error_messages = {
-        'inactive_account': _('User account is disabled.'),
-        'invalid_credentials': _('Unable to login with provided credentials.')
-    }
-
-    def __init__(self, *args, **kwargs):
-        super(UserLoginSerializer, self).__init__(*args, **kwargs)
-        self.user = None
-
-    def validate(self, attrs):
-        self.user = authenticate(username=attrs.get("username"), password=attrs.get('password'))
-        if self.user:
-            if not self.user.is_active:
-                raise serializers.ValidationError(self.error_messages['inactive_account'])
-            return attrs
-        else:
-            raise serializers.ValidationError(self.error_messages['invalid_credentials'])
 
 
 class TokenSerializer(serializers.ModelSerializer):
@@ -44,4 +21,3 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
         fields = ("token", )
-
--- myapp/user/handlers.py
+++ myapp/user/handlers.py
@@ -7,7 +7,7 @@ from django.views.generic.base import TemplateView
 from myapp.user.requests import *
 from myapp.user.usecases import *
 from shared.base_handler import *
-from myapp.serializer.auth_serializer import UserLoginSerializer, TokenSerializer, UserSerializer
+from myapp.serializer.auth_serializer import TokenSerializer, UserSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -19,38 +19,34 @@ class AuthHandler(TemplateView):
     usecase = inject.attr(UsecaseInterface)
     bh = inject.attr(BaseHandler)
 
-    def post(self, request):
-        response = self.bh.validate(UserRegister, request.POST)
-        if response is not None:
-            return response
-        context = {"message": "validate success"}
-        return render(request, 'base.html', context)
+    # def post(self, request):
+    #     response = self.bh.validate(UserRegister, request.POST)
+    #     if response is not None:
+    #         return response
+    #     context = {"message": "validate success"}
+    #     return render(request, 'base.html', context)
 
-    def get(self, request):
-        # response = self.bh.validate(UserRegister, request.POST)
-        context = {"message": "validate success"}
-        return render(request, 'authen/register.html', context)
+    # def get(self, request):
+    #     # response = self.bh.validate(UserRegister, request.POST)
+    #     context = {"message": "validate success"}
+    #     return render(request, 'authen/register.html', context)
 
 
 class UserLoginAPIView(GenericAPIView):
-    authentication_classes = ()
-    permission_classes = ()
+    bh = inject.attr(BaseHandler)
     serializer_class = UserLoginSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.user
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response(
-                data=TokenSerializer(token).data,
-                status=status.HTTP_200_OK,
-            )
-        else:
-            return Response(
-                data=serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        response = self.bh.validate(serializer)
+        if response is not None:
+            return response
+        user = serializer.user
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response(
+            data=TokenSerializer(token).data,
+            status=status.HTTP_200_OK,
+        )
 
 
 class UserInfoAPIView(GenericAPIView):
