351783d80b7a9a8c2bf9dd9bab7234877f96d84d
Vu Dang N <vu.dang@asiantech.vn>
2019-01-17 14:56:04 +0700

    implement-token-rest-framework

--- myapp/user/handlers.py
+++ myapp/user/handlers.py
@@ -1,11 +1,17 @@
-import inject, json, logging
+import inject
+import json
+import logging
 
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from myapp.user.requests import *
 from myapp.user.usecases import *
 from shared.base_handler import *
-
+from myapp.serializer.auth_serializer import UserLoginSerializer, TokenSerializer, UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from myapp.permission.user_permission import IsAdminUser
 logger = logging.getLogger(__name__)
 
 
@@ -24,3 +30,33 @@ class AuthHandler(TemplateView):
         # response = self.bh.validate(UserRegister, request.POST)
         context = {"message": "validate success"}
         return render(request, 'authen/register.html', context)
+
+
+class UserLoginAPIView(GenericAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.user
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response(
+                data=TokenSerializer(token).data,
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                data=serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
+class UserInfoAPIView(GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+
+    def get(self, request):
+        serializer = self.serializer_class(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
