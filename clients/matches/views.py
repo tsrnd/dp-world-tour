8c9c767eef4f688c7f79142b75a788f1b845d8a3
Vu Dang N <vu.dang@asiantech.vn>
2019-01-23 09:57:42 +0700

    Fix bug min today

--- clients/matches/views.py
+++ clients/matches/views.py
@@ -7,13 +7,14 @@ from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
 def index(request):
-    toDay = datetime.now().strftime("%Y-%m-%d")
-    response = render(request, 'match/match.html', {'today': toDay})
+    today = datetime.now().strftime("%Y-%m-%d")
+    response = render(request, 'match/match.html', {'today': today})
     return response
 
 
 def find_match(request):
     token = request.COOKIES.get('token')
+    today = datetime.now().strftime("%Y-%m-%d")
     if token != None:
         if request.method == 'POST':
             form = FindMatchForm(request.POST)
@@ -24,13 +25,12 @@ def find_match(request):
                     'date_match': date_match,
                 }, headers=headers)
                 response = r.json()
-                print(response)
                 if r.status_code == 201:
                     messages.add_message(request, messages.SUCCESS, _('Đã đăng kí tìm kiếm trận đấu thành công.'))
                 elif r.status_code == 400:
                     messages.add_message(request, messages.ERROR, _('Mời bạn chọn lịch thi đấu khác'))
                 else:
                     messages.add_message(request, messages.ERROR, _('Tìm kiếm trận đấu thất bại.'))
-                return render(request, 'match/match.html', None)
+                return render(request, 'match/match.html', {'today': today})
     messages.add_message(request, messages.ERROR, _('Tìm kiếm trận đấu thất bại.'))
-    return render(request, 'match/match.html', {'form': form})
+    return render(request, 'match/match.html', {'form': form, 'today': today})
