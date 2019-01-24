aa3bb83ef7bc7b95593569437080239041796f46
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-24 11:34:37 +0700

    beautify & link url

--- clients/teams/urls.py
+++ clients/teams/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from clients.teams import views
 
 urlpatterns = [
-    path('register', views.register),
-    path('list', views.list),
+    path('register', views.register, name='team-register'),
+    path('list', views.list, name='team-list'),
 ]
