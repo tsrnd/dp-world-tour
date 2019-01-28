68e1a03eb0f9a12047e0246735312fe7145efda2
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-28 15:19:07 +0700

    implement accept/reject math with sendmail

--- myapp/match/urls.py
+++ myapp/match/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from myapp.match.handlers import FindMatchAPIView
+from django.conf.urls import url
+from myapp.match.handlers import (
+    FindMatchAPIView,
+    MatchUpdate,
+)
 urlpatterns = [
     path('', FindMatchAPIView.as_view(), name='find-match'),
+    url(r'^(?P<pk>\d+)$', MatchUpdate.as_view(), name='match-update'),
 ]
