580f0a6e69ac55503fa4a9d680668c52ca1d496e
Lam Le V <lam.le@asiantech.vn>
2019-02-11 12:40:02 +0700

    Add query for template

--- clients/booking/urls.py
+++ clients/booking/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 from clients.booking import views
 
 urlpatterns = [
-    re_path('list/(?:page-(?P<page>\d+)/)?$', views.my_list_booking, name='list')
-    
+    re_path('list/(?:page-(?P<page>\d+)/)?$',
+            views.my_list_booking, name='list')
+
 ]
