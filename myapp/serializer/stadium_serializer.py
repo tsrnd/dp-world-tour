e38132e3ee7f6971290c60e0227d635185b8fb44
Lam Le V <lam.le@asiantech.vn>
2019-02-12 14:45:05 +0700

    Check iterable in code

--- myapp/serializer/stadium_serializer.py
+++ myapp/serializer/stadium_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from myapp.models.stadiums import Stadium
+from collections.abc import Iterable
 
 
 class StadiumSerializer(serializers.ModelSerializer):
@@ -10,7 +11,7 @@ class StadiumSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super(StadiumSerializer, self).__init__(*args, **kwargs)
-        if len(args[0]) > 0 and 'exclude_fields' in args[0]:
+        if isinstance(args[0], Iterable) and len(args[0]) > 0 and 'exclude_fields' in args[0]:
             fields = args[0]
             for field_name in fields['exclude_fields']:
                 self.fields.pop(field_name)
